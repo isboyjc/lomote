965e82a8745573195ae9223b3e34b43314351a47
isboyjc <214930661@qq.com>
2025-03-04 02:23:39 +0800

    feat: 配置 Nuxt Content 预览 API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  },
+
   nitro: {
     preset: "cloudflare-pages"
   },
