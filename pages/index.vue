84e9a20ea9cda441694c5b15cced91a37d815508
isboyjc <214930661@qq.com>
2025-03-04 03:42:04 +0800

    chore: 移除首页调试日志

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
 
-console.log(4, home)
-
 useSeoMeta({
   title: home.value?.title,
   description: home.value?.description
