74c305bd8f2d0e41359bc27b4f6d1d3c8d662992
isboyjc <214930661@qq.com>
2025-03-04 06:50:25 +0800

    feat(nuxt): 增强项目配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,24 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  nitro: {
+    preset: "cloudflare-pages"
+  },
+
   content: {
     database: {
       type: 'd1',
       bindingName: 'DB'
+    },
+    preview: {
+      api: 'https://api.nuxt.studio'
     },
     build: {
       markdown: {
@@ -15,28 +27,57 @@ export default defineNuxtConfig({
           searchDepth: 3
         },
         highlight: {
+          langs: ['json', 'js', 'ts', 'html', 'css', 'vue', 'shell', 'mdc', 'md', 'yaml'],
           theme: {
             default: 'github-light',
             dark: 'github-dark',
-            sepia: 'monokai'
-          }
-        }
+            sepia: 'monokai',
+          },
+        },
       }
     }
-  },
-
-  nitro: {
-    preset: "cloudflare-pages"
   },
 
   modules: [
     "nitro-cloudflare-dev",
     "@nuxtjs/tailwindcss",
     "shadcn-nuxt",
-    "@nuxt/content"
+    "@nuxt/content",
+    '@nuxt/icon',
+    '@vueuse/nuxt',
   ],
   shadcn: {
-    prefix: '',
+    prefix: 'Ui',
     componentDir: './components/ui'
-  }
+  },
+  components: {
+    dirs: [
+      {
+        path: './components',
+        ignore: ['**/*.ts'],
+      },
+    ],
+  },
+  css: [
+    join(currentDir, './assets/css/themes.css'),
+  ],
+  icon: {
+    clientBundle: {
+      scan: true,
+      sizeLimitKb: 512,
+    },
+    customCollections: [
+      {
+        prefix: '4agi',
+        dir: './assets/icons',
+      },
+    ],
+  },
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        baseUrl: '.',
+      },
+    },
+  },
 })
